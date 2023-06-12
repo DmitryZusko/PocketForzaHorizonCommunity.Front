21593dcf73a07c148cdec1d6cb01e938bf65dc4b
Dmitry <elxf123@gmail.com>
2023-06-12 11:36:05 +0300

    refactoring: change a sing-in form validation a bit, now the password is only required, but do not checks other constraints

--- src/components/forms/SingInFormComponent/constants.ts
+++ src/components/forms/SingInFormComponent/constants.ts
@@ -5,13 +5,5 @@ export const validationScheme = Yup.object({
     .email("Please, enter a valid email")
     .max(255, "Email length shouldn't exceed 255 characters")
     .required("Please, enter your email"),
-  password: Yup.string()
-    .min(8, "Password should be at least 8 characters long")
-    .max(64, "Password length shouldn't exceed 64 characters")
-    .matches(/[0-9]/g, "Password should contain a number")
-    .matches(/[a-z]/g, "Password should contain a lowercase letter")
-    .matches(/[A-Z]/g, "Password should contain an uppercase letter")
-    .matches(/\W/g, "Password should contain a symbol")
-    .matches(/^\S*$/g, "Password shouldn't contain whitespaces")
-    .required("Please, enter a password"),
+  password: Yup.string().required("Please, enter a password"),
 });
